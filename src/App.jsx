e1f90f9bd220732771a77ee9e93c30f721fa2bf0
kik87827 <kik87827@gmail.com>
2025-10-16 12:51:10 +0900

    스터디중

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let post = "강남 우동 맛집";
+  let [article, setArticle] = useState([
+    {
+      title: "남자 코트 추천",
+      date: "2월 17일",
+    },
+    {
+      title: "강남 우동맛집",
+      date: "2월 17일",
+    },
+    {
+      title: "파이썬독학",
+      date: "2월 17일",
+    },
+  ]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="black-nav">
+        <h2>블로그임</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="list-wrap">
+        {article.map((item, idx) => {
+          return (
+            <div className="list-item" key={item.title}>
+              <h4>{item.title}</h4>
+              <p>{item.date} 발행</p>
+            </div>
+          );
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <h4>{post}</h4>
+    </div>
+  );
 }
 
-export default App
+export default App;
